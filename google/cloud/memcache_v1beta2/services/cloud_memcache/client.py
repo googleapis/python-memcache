f02f8315c279c41cf150b00fa59666ad6c0f67d4
Anthonios Partheniou <partheniou@google.com>
2021-02-09 13:38:06 +0000

    Fix typo

--- google/cloud/memcache_v1beta2/services/cloud_memcache/client.py
+++ google/cloud/memcache_v1beta2/services/cloud_memcache/client.py
@@ -559,8 +559,7 @@ class CloudMemcacheClient(metaclass=CloudMemcacheClientMeta):
                 -  Must start with a letter.
                 -  Must be between 1-40 characters.
                 -  Must end with a number or a letter.
-                -  Must be unique within the user project / location
-.
+                -  Must be unique within the user project / location.
 
                 If any of the above are not met, the API raises an
                 invalid argument error.
