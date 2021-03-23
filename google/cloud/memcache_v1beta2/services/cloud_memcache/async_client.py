885e8e1180d239d1bcdc6d0e5cf7c93e8c3c49c5
yoshi-automation <yoshi-automation@google.com>
2021-03-23 15:33:25 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google/cloud/memcache_v1beta2/services/cloud_memcache/async_client.py
+++ google/cloud/memcache_v1beta2/services/cloud_memcache/async_client.py
@@ -100,8 +100,36 @@ class CloudMemcacheAsyncClient:
         CloudMemcacheClient.parse_common_location_path
     )
 
-    from_service_account_info = CloudMemcacheClient.from_service_account_info
-    from_service_account_file = CloudMemcacheClient.from_service_account_file
+    @classmethod
+    def from_service_account_info(cls, info: dict, *args, **kwargs):
+        """Creates an instance of this client using the provided credentials info.
+
+        Args:
+            info (dict): The service account private key info.
+            args: Additional arguments to pass to the constructor.
+            kwargs: Additional arguments to pass to the constructor.
+
+        Returns:
+            CloudMemcacheAsyncClient: The constructed client.
+        """
+        return CloudMemcacheClient.from_service_account_info.__func__(CloudMemcacheAsyncClient, info, *args, **kwargs)  # type: ignore
+
+    @classmethod
+    def from_service_account_file(cls, filename: str, *args, **kwargs):
+        """Creates an instance of this client using the provided credentials
+        file.
+
+        Args:
+            filename (str): The path to the service account private key json
+                file.
+            args: Additional arguments to pass to the constructor.
+            kwargs: Additional arguments to pass to the constructor.
+
+        Returns:
+            CloudMemcacheAsyncClient: The constructed client.
+        """
+        return CloudMemcacheClient.from_service_account_file.__func__(CloudMemcacheAsyncClient, filename, *args, **kwargs)  # type: ignore
+
     from_service_account_json = from_service_account_file
 
     @property
@@ -357,7 +385,8 @@ class CloudMemcacheAsyncClient:
                 -  Must start with a letter.
                 -  Must be between 1-40 characters.
                 -  Must end with a number or a letter.
-                -  Must be unique within the user project / location.
+                -  Must be unique within the user project / location
+.
 
                 If any of the above are not met, the API raises an
                 invalid argument error.
